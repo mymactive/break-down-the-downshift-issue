5a57482d5aa7e593c038553595f0c976409c5f0b
@mymactive <mymactive@gmail.com>
2022-10-31 01:39:45 +0900

    feat: Make index page

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import { SimpleExample } from 'components/SimpleExample'
+
+export const App: FC = () => (
+  <div className="flex gap-2">
+    <SimpleExample />
+  </div>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
+import { App } from 'components/App'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<h1>Hello World</h1>)
+root.render(<App />)
